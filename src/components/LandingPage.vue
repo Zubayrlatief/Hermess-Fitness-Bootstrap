764e5c0692df58dfd7d52e9b576e5533ea713426
Zubayr-Latief <zubayrlatie007@gmail.com>
2024-08-01 16:09:41 +0200

    nav

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
-        LandingPage
+  <section class="section">
+    <div class="container-image">
+        <img class="landing-image" src="https://zubayrlatief.github.io/hosted-images/IMG_2337.jpg" alt="image">
+        <div class="container-text">
+            <div class="container-text">
+                
+            </div>
+        </div>
     </div>
+  </section>
 </template>
 <script>
 export default {
@@ -9,5 +16,14 @@ export default {
 }
 </script>
 <style>
-    
+.section{
+    background-color: #040404;
+}
+.container-image{
+    margin-left: 2rem;
+}
+.landing-image{
+    width: 35%;
+    height: 50%;
+}
 </style>
